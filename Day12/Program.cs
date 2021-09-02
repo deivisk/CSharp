ddbcf79586732d0bb58f132e20033c708c621ba1
deivisk <davis.kal@outlook.com>
2021-09-02 21:01:35 +0300

    Upadtes

---
+++ Day12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kkas = new string[5];
+
+            for(int i = 0; i < kkas.Length; i++)
+            {
+                Console.WriteLine("Ievadiet frāzi");
+
+                string ievade = Console.ReadLine();
+
+                kkas[i] = ievade;
+
+                if(ievade.Contains("sveiki") == true)
+                {
+                   kkas[i] = "*****";
+                }
+
+            Console.WriteLine(kkas[i]);
+
+            }
+
+
+
+        }
+    }
+}

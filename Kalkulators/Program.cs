c5f29139b434e4c19c1a1aecf5793abce7728fca
deivisk <davis.kal@outlook.com>
2021-09-08 20:11:21 +0300

    Kalkulators

---
+++ Kalkulators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kalkulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            string c = Console.ReadLine();
+
+            if(c == "+")
+            {
+                a = a + b;
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}

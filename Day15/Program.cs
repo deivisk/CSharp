3c0819aaa477c79dc0f81c4b52350b46d2329067
deivisk <davis.kal@outlook.com>
2021-09-16 19:36:50 +0300

    Saraksts

---
+++ Day15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            List<int> saraksts = new List<int>();
+
+            int randomnumber = 0;
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < 30; i++)
+            {
+                randomnumber = random.Next(0, 11);
+
+                saraksts.Add(randomnumber);
+            }
+            if(input)
+            {
+                for(int i = 0; i < saraksts.Count; i++)
+                {
+                    Console.WriteLine(saraksts[input]);
+                }
+            }
+
+        }
+    }
+}

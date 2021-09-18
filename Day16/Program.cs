c2bf09ece5ea793a53098141269482c9700589f5
deivisk <davis.kal@outlook.com>
2021-09-18 10:55:51 +0300

    Listu salīdzināšana

---
+++ Day16/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("");
+
+            if()
+            {
+
+            }
+
+        }
+        private static int User()
+        {
+            List<int> UserList = new List<int>();
+
+            int input = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ievadiet skaitli");
+
+                input = Convert.ToInt32(Console.ReadLine());
+
+                UserList.Add(input);
+            }
+        }
+
+        private static int RANAI()
+        {
+            List<int> AIList = new List<int>();
+
+            Random random = new Random();
+
+            int randomnumber = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                randomnumber = random.Next(1, 10);
+
+                AIList.Add(randomnumber);
+            }
+            
+        }
+    }
+}

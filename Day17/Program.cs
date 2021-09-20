83cd6d39d7997da653a55dc7d9908fdfb367fd0c
deivisk <davis.kal@outlook.com>
2021-09-20 19:24:29 +0300

    String liste

---
+++ Day17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day17
+{
+    class Program
+    {
+        static List<string> first = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Izvēlaties, cik elementus ievadīt");
+
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            if(count < 1)
+            {
+                count = 5;
+
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Ievadiet elementu");
+
+                String ievade = Console.ReadLine();
+
+
+            }
+        }
+    }
+}

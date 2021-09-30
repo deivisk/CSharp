5400c717c10277ab299fa112c4fc904064eeca73
deivisk <davis.kal@outlook.com>
2021-09-30 19:33:53 +0300

    +1

---
+++ GUI2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+
+            int b = Convert.ToInt32(label1.Text);
+
+            int c = b + a;
+
+            label1.Text = Convert.ToString(c);
+        }
+    }
+}

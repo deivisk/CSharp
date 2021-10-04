b008f79b6cbf2e4698555932fbb396b0c00b389b
deivisk <davis.kal@outlook.com>
2021-10-04 14:19:43 +0300

    Update Form1.Designer.cs

--- GUI3/Form1.Designer.cs
+++ GUI3/Form1.Designer.cs
@@ -129,6 +129,7 @@ namespace GUI3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(813, 308);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.Pievienot3);

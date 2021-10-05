e6f5ac1b3c3af4a8bda7b42cf6f7e75d14dbf972
deivisk <davis.kal@outlook.com>
2021-10-05 20:59:09 +0300

    Input

---
+++ Day20_IO(Input, Output)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Day20_IO_Input__Output_
+{
+    class Program
+    {
+        static string filepath = @"C:\Users\deivi\Desktop\C# kursi";
+        static void Main(string[] args)
+        {
+
+            String fileName = filepath + "piemers.txt";
+
+           try
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+
+                Console.WriteLine("Ievadi pirmo teksu!");
+                string pirmais = Console.ReadLine();
+
+                Console.WriteLine("Ievadi otro teksu!");
+                string otrais = Console.ReadLine();
+
+                Console.WriteLine("Ievadi trešo teksu!");
+                string trešais = Console.ReadLine();
+
+                writer.WriteLine(pirmais);
+                writer.WriteLine(otrais);
+                writer.WriteLine(trešais);
+                writer.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Kļūda!");
+            }
+        }
+    }
+}

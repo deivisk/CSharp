eac9914720f862760b5fe7b685eb4ade97baec9a
deivisk <davis.kal@outlook.com>
2021-10-07 20:29:56 +0300

    Nestrādā

---
+++ Day21/ListSamples.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day21
+{
+    class ListSamples
+    {
+        static String folderPath = @"C:\temp\";
+        
+        
+        public static void Sample1()
+        {
+            List<String> Liste = new List<String>();
+
+            Random random = new Random();
+
+
+
+            String fileName = folderPath + "piemeri.txt";
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+
+                for(int i = 0; i < 20; i++)
+                {
+                    int randomnum = random.Next(0, 21);
+
+                    writer.WriteLine(randomnum);
+                }
+
+                writer.Close();
+
+            }
+            catch
+            {
+                Console.WriteLine("Fails neeksistē");
+            }
+        }
+    }
+}

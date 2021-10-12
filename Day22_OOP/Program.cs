406b331102b31a370c2137220496616dcaa5fa0f
deivisk <davis.kal@outlook.com>
2021-10-12 20:40:57 +0300

    Update Program.cs

--- Day22_OOP/Program.cs
+++ Day22_OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day22_OOP
 {
@@ -7,19 +8,41 @@ namespace Day22_OOP
         static void Main(string[] args)
         {
             Random random = new Random();
+            List<Candidate> Liste = new List<Candidate>();
 
             Double randomnum = random.Next(0, 11);
             Double randomnumb = random.Next(0, 11);
 
             double avarage = (randomnum + randomnumb) / 2;
 
+            Console.WriteLine("Cik studentus ievadīt?");
+            int daudzums = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Studenta vārds");
+            string nn = Console.ReadLine();
+
+            Console.WriteLine("Studenta uzvārds");
+            string nna = Console.ReadLine();
+
+
+
             Candidate first = new Candidate("Pēteris ", "Gulbis", randomnumb, randomnum, avarage);
             first.izvade();
 
             Console.WriteLine();
 
-            Candidate second = new Candidate("Solvita ", "Āboltiņa", randomnumb, randomnumb, avarage);
+            Candidate second = new Candidate("Solvita ", "Āboltiņa", randomnumb, randomnum, avarage);
             second.izvade();
+
+            Console.WriteLine();
+
+            Candidate inf = new Candidate(nn, nna, randomnumb, randomnum, avarage);
+            inf.izvade();
+
+            for(int i = 0; i < daudzums; i++)
+            {
+                Liste.Add(inf);
+            }
         }
     }
 }

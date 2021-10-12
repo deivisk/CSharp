cdf86b3cc12e9616f3bf4ece9ad630c8f5604cc6
deivisk <davis.kal@outlook.com>
2021-10-12 19:44:13 +0300

    HR

---
+++ Day22_OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day22_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double randomnum = random.Next(0, 11);
+
+            kautkas first = new kautkas();
+
+            kautkas second = new kautkas();
+        }
+    }
+}

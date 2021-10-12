473593773caacd31354b7d866b7d6fa0350c95ae
deivisk <davis.kal@outlook.com>
2021-10-12 19:47:29 +0300

    Update Candidate.cs

--- Day22_OOP/Candidate.cs
+++ Day22_OOP/Candidate.cs
@@ -6,5 +6,23 @@ namespace Day22_OOP
 {
     class Candidate
     {
+        public String name;
+        public String LastName;
+        public Double HRmark;
+        public Double TVmark;
+
+        public kautkas(String name, String LastName, Double HRmark, Double TVmark)
+        {
+            this.name = name;
+            this.LastName = LastName;
+            this.HRmark = HRmark;
+            this.TVmark = TVmark;
+        }
+        public static void izvade()
+        {
+            Console.WriteLine("vārds, uzvārds " + name + LastName);
+            Console.WriteLine("Dotā atzīme no HR - " + HRmark);
+            Console.WriteLine("Dotā atzīme no tehniskā vadītāja - " TVmark);
+        }
     }
 }

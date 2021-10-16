8b9d46cec8fd684d6a109b6ef33249df1b1c7775
deivisk <davis.kal@outlook.com>
2021-10-16 14:28:41 +0300

    Trijsturis

---
+++ Day23_OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day23_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri = new Triangle();
+            for(int i =0)
+            Console.WriteLine("Ievadi izmēru malai");
+            tri.FirstSide = Convert.ToInt32(Console.ReadLine());
+
+            
+        }
+    }
+}
---
+++ Day23_OOP/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23_OOP
+{
+    class Triangle
+    {
+        public int FirstSide
+        {
+            get
+            {
+                return FirstSide;
+            }
+            set
+            {
+                if(FirstSide + SecondSide < ThridSide)
+                {
+                    FirstSide = 0;
+                }
+                else if(FirstSide + ThridSide < SecondSide)
+                {
+                    FirstSide = 0;
+                }
+                else
+                {
+                    FirstSide = value;
+                }
+            }
+        }
+        public int SecondSide { get; set; }
+        public int ThridSide { get; set; }
+
+        public void Tri()
+        {
+            this.FirstSide = FirstSide;
+            this.SecondSide = SecondSide;
+            this.ThridSide = ThridSide;
+        }
+
+        public void Izvade()
+        {
+            if((FirstSide + SecondSide + ThridSide) > 0)
+            {
+                Console.WriteLine("Jūsu trijstūris ir pareizs!");
+            }
+            else
+            {
+                Console.WriteLine("Trijustūris neeksistē");
+            }
+
+        }
+    }
+}

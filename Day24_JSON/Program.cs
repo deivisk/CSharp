f3b1c8f1d30eaf54c43c4a1f59064346cbba48fa
deivisk <davis.kal@outlook.com>
2021-10-23 11:15:22 +0300

    ...

---
+++ Day24_JSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Day24_JSON
+{
+    class Program
+    {
+        static string filepath = @"C:\Users\deivi\Downloads\";
+        static void Main(string[] args)
+        {
+            string fileName = filepath + "piemers.txt";
+            User inf = new User() ;
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(fileName);
+
+                inf.InitUser("abc","abc", "abc", "abc");
+                String jsonString = JsonSerializer.Serialize(inf);
+                writer.WriteLine(jsonString);
+
+                writer.Close();
+            }
+            catch
+            {
+                Console.WriteLine("404");
+            }
+
+        }
+    }
+}
---
+++ Day24_JSON/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day24_JSON
+{
+    class User
+    {
+        String Nick { get; set; }
+        String pass { get; set; }
+        String name { get; set; }
+        String last { get; set; }
+
+        public void InitUser(String Nick, String pass, String name, String last)
+        {
+            this.Nick = Nick;
+            this.pass = pass;
+            this.name = name;
+            this.last = last;
+
+        }
+        public void print()
+        {
+            Console.WriteLine(Nick + " " + pass + " " + name + " " + last);
+        }
+    }
+}

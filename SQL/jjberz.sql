2c40194855eb42b8346db65824f9d82deeff935d
deivisk <davis.kal@outlook.com>
2021-11-03 21:12:08 +0200

    finished

--- SQL/jjberz.sql
+++ SQL/jjberz.sql
@@ -1,17 +1,21 @@
 Create table Customers (
-	ClientType INT Primary Key Auto_Increment,
+	ClientID INT Primary Key Auto_Increment,
+ClientType int,
 FirstName VarChar(20) Not Null,
 LastName VarChar(20) Not Null,
 PhoneNum Int,
 Email VarChar (30) Not Null
 );
 
-Insert Into Customers (ClientType, FirstName, LastName, PhoneNum, Email)
-Values (1, 'Janis', 'Berzins', 29293982, 'jjberzins@gmail.com'),
-		(2, 'Juris', 'Berzins', 29293982, 'jjberzins@gmail.com'),
-        ('Janchuks', 'Berzins', 29293982, 'jjberzins@gmail.com'),
-        ('Jecis', 'Bierzins', 29293982, 'jjbierzins@gmail.com'),
-        ('Jekabs', 'Bierins', 29293982, 'jjbierins@gmail.com');
+Drop Table Customers;
+
+
+Insert Into Customers (FirstName, LastName, PhoneNum, Email, ClientType)
+Values ('Janis', 'Berzins', 29293982, 'jjberzins@gmail.com', 1),
+		('Juris', 'Berzins', 29293982, 'jjberzins@gmail.com', 2),
+        ('Janchuks', 'Berzins', 29293982, 'jjberzins@gmail.com', 2),
+        ('Jecis', 'Bierzins', 29293982, 'jjbierzins@gmail.com', 1),
+        ('Jekabs', 'Bierins', 29293982, 'jjbierins@gmail.com', 3);
         
 Select * From Customers;
 

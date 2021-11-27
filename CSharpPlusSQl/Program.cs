2c4f7f058d9da330c74196e532f00884a7fe10a5
deivisk <davis.kal@outlook.com>
2021-11-27 12:49:53 +0200

    JaunaSchema

---
+++ CSharpPlusSQl/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CSharpPlusSQl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            connection();
+        }
+
+
+        static void connection()
+        {
+            var settings = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                UserID = "root",
+                Password = "danger123",
+                Port = 3306,
+                Database = "SQL!!"
+            };
+
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(settings.ConnectionString);
+                conn.Open();
+
+                Console.WriteLine("Connection successful");
+            }
+            catch
+            {
+                Console.WriteLine("Connection failed!");
+
+            }
+        }
+    }
+}
---
+++ JaunaSchema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JaunaSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
